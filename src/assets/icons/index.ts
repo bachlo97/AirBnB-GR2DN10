61133ac9d8d398f5ac007c7a265ad24c207a86e8
bachlo97 <cuongpt8997@gmail.com>
2024-05-11 20:03:21 +0700

    auth 70%

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { EmailIcon } from "./email.icon";
+export { ClosedEyeIcon } from "./closed-eye.icon";
+export { OpendEyeIcon } from "./opened-eye.icon";
+export { UserIcon } from "./user.icon";
+export {PhoneIcon} from "./phone.icon"
