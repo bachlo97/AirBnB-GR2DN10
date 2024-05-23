c74b2d4456b2ce2a2913f47485323504e9e101d6
bachlo97 <cuongpt8997@gmail.com>
2024-05-24 06:04:07 +0700

    user/admin 60%

--- src/pages/admin/user-management/modal/form-modal.tsx
+++ src/pages/admin/user-management/modal/form-modal.tsx
@@ -1,33 +1,80 @@
 import React, { useState } from "react";
-import { Modal } from "antd";
+import { Form, Input, Modal } from "antd";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { closeModal } from "@/redux/admin/user-management/user-management.slice";
 type Props = {
-  // show: any,
+  title: string,
+  okText: string,
 };
 
-export  function FormModal({}: Props) {
-  const modal = useAppSelector(state => state.userManagementReducer.modal)
-  const dispatch = useAppDispatch()
+export function FormModal({title,}: Props) {
+  const modal = useAppSelector((state) => state.userManagementReducer.modal);
+  const dispatch = useAppDispatch();
 
   const handleOk = () => {
-    dispatch(closeModal())
+    dispatch(closeModal());
+    alert(123);
   };
 
   const handleCancel = () => {
-    dispatch(closeModal())
+    dispatch(closeModal());
   };
   return (
     <>
       <Modal
-        title="Basic Modal"
+        title={<h2 className='text-[20px]'>Thêm quản trị viên</h2>}
         open={modal}
         onOk={handleOk}
         onCancel={handleCancel}
+        okText="Thêm"
+        cancelText="Huỷ"
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <div className="flex justify-center gap-8">
+          <div className="w-[100%]">
+            <label htmlFor="">Name</label>
+            <Input />
+          </div>
+        </div>
+
+
+
+        <div className="mt-5 flex gap-8">
+          <div className="w-[50%]">
+            <label className="text-left" htmlFor="">
+              Phone number
+            </label>
+            <Input />
+          </div>
+          <div className="w-[50%]">
+            <label className="text-left" htmlFor="">
+              birthday
+            </label>
+            <Input />
+          </div>
+        </div>
+
+        <div className="w-[100%]">
+          <label htmlFor="">Email</label>
+          <Input />
+        </div>
+
+        <div className="mt-5 flex gap-8">
+          <div className="w-[50%]">
+            <label className="text-left" htmlFor="">
+              Gender
+            </label>
+            <Input />
+          </div>
+          <div className="w-[50%]">
+            <label className="text-left" htmlFor="">
+              Role
+            </label>
+            <Input />
+          </div>
+        </div>
+
+
+
       </Modal>
     </>
   );
--- src/pages/admin/user-management/user-management.tsx
+++ src/pages/admin/user-management/user-management.tsx
@@ -1,18 +1,22 @@
-import { getUsersThunk, openModal, searchUsersThunk } from '@/redux/admin/user-management/user-management.slice'
-import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import {
+  getUsersThunk,
+  openModal,
+  searchUsersThunk,
+} from "@/redux/admin/user-management/user-management.slice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import type { TableColumnsType, TableProps } from "antd";
-import React, { useEffect, useRef, useState } from 'react'
-import { Table, Input, Button, Popconfirm,Tag, Modal } from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import { Table, Input, Button, Popconfirm, Tag, Modal } from "antd";
 import { IoSearchOutline } from "react-icons/io5";
 import { IoIosMale } from "react-icons/io";
 import { IoFemaleOutline } from "react-icons/io5";
 import { VscEdit } from "react-icons/vsc";
 import { TiDelete } from "react-icons/ti";
 import type { SearchProps } from "antd/es/input/Search";
-import { truncateText } from '@/utils';
-import { FormModal } from './modal/form-modal';
-import { getUsers, searchUsers } from '@/services/user';
-type Props = {}
+import { truncateText } from "@/utils";
+import { FormModal } from "./modal/form-modal";
+import { getUsers, searchUsers } from "@/services/user";
+type Props = {};
 
 interface DataType {
   key: React.Key;
@@ -26,154 +30,148 @@ interface DataType {
 }
 
 export default function UserManagement({}: Props) {
-  const {Search} = Input;
+  const { Search } = Input;
   const userRef = useRef<any>(null);
-  const dispatch = useAppDispatch()
-  const {userList} = useAppSelector(state => state.userManagementReducer)
-  const [value, setValue] = useState("");
-  console.log({userList})
-  useEffect(()=>{
-    dispatch(getUsersThunk())
-  },[])
+  const dispatch = useAppDispatch();
+  const { userList } = useAppSelector((state) => state.userManagementReducer);
+  // const [value, setValue] = useState("");
+  console.log({ userList });
+  useEffect(() => {
+    dispatch(getUsersThunk());
+  }, []);
 
-
-
-const columns: TableColumnsType<DataType> = [
-  {
+  const columns: TableColumnsType<DataType> = [
+    {
       title: "STT",
       dataIndex: "stt",
       sorter: (a, b) => a.stt - b.stt,
       // sortDirections: ["descend", "ascend"],
-  },
+    },
 
-  {
+    {
       title: "Name",
       dataIndex: "name",
       // sorter: (a, b) => a.name.length - b.name.length,
       sorter: (a, b) => a.name.localeCompare(b.name),
-  },
-  {
+    },
+    {
       title: "Email",
       dataIndex: "email",
-      render: (_,{email}) =>(
-        <>{truncateText(email,23)}</>
-      )
-
-  },
-  {
+      render: (_, { email }) => <>{truncateText(email, 23)}</>,
+    },
+    {
       title: "Birthday",
       dataIndex: "birthday",
-  },
-  {
+    },
+    {
       title: "Gender",
       dataIndex: "gender",
-      sorter: (a,b) => a.gender.length - b.gender.length,
-      render: (_,{gender}) =>
-        (
-          <div className='font-bold text-[20px]'>
-          {gender === 'male' ? <IoIosMale className='text-blue-500' /> : <IoFemaleOutline className='text-pink-500' /> }
-          </div>
-        )
-  },
-  {
-    title:'Role',
-    dataIndex: 'role',
-    sorter: (a,b) => a.role.length - b.role.length,
-    render: (_,{role}) =>(
-     <Tag color = {role.length > 4 ? 'purple' : 'cyan'}>
-      {role.toUpperCase()}
-     </Tag>
+      sorter: (a, b) => a.gender.length - b.gender.length,
+      render: (_, { gender }) => (
+        <div className="text-[20px] font-bold">
+          {gender === "male" ? (
+            <IoIosMale className="text-blue-500" />
+          ) : (
+            <IoFemaleOutline className="text-pink-500" />
+          )}
+        </div>
+      ),
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+      sorter: (a, b) => a.role.length - b.role.length,
+      render: (_, { role }) => (
+        <Tag color={role.length > 4 ? "purple" : "cyan"}>
+          {role.toUpperCase()}
+        </Tag>
+      ),
+    },
 
-    )
-  },
-
-  {
+    {
       title: "Action",
       dataIndex: "action",
       render: (_, { action }) => {
         return (
-            <div className='flex gap-4'>
-              <div className='self-center text-blue-500 cursor-pointer' onClick={()=> dispatch(openModal())}>
-              <VscEdit/>
-              </div>
-               
-                <Popconfirm
-                    title="Bạn có muốn xóa "
-                    onConfirm={async () => {
-                        // await dispatch(deleteUserThunk(taiKhoan));
-                        // dispatch(getUsersThunk(""));
-                    }}
-                    cancelText="Huỷ"
-                    okText="Chắn chắn"
-                >
-                    <span
-                        className={
-                            " mr-3 cursor-pointer text-[20px] text-red-500"
-                        }
-                        onClick={async () => {}}
-                    >
-                      <TiDelete/>
-                    </span>
-                </Popconfirm>
+          <div className="flex gap-4">
+            <div
+              className="cursor-pointer self-center text-blue-500"
+              onClick={() => dispatch(openModal())}
+            >
+              <VscEdit />
             </div>
+
+            <Popconfirm
+              title="Bạn có muốn xóa "
+              onConfirm={async () => {
+                // await dispatch(deleteUserThunk(taiKhoan));
+                // dispatch(getUsersThunk(""));
+              }}
+              cancelText="Huỷ"
+              okText="Chắn chắn"
+            >
+              <span
+                className={" mr-3 cursor-pointer text-[20px] text-red-500"}
+                onClick={async () => {}}
+              >
+                <TiDelete />
+              </span>
+            </Popconfirm>
+          </div>
         );
+      },
     },
-     
-  },
-];
+  ];
 
-const data = userList.map((item: DataType, index) => ({
-  ...item,
-  stt: index + 1,
-  gender: item.gender ? 'male' : 'female',
-  role: item.role
-}));
-console.log({data})
+  const data = userList.map((item: DataType, index) => ({
+    ...item,
+    stt: index + 1,
+    gender: item.gender ? "male" : "female",
+    role: item.role,
+  }));
+  console.log({ data });
 
-const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
-  console.log('params', pagination, filters, sorter, extra);
-};
-
+  const onChange: TableProps<DataType>["onChange"] = (
+    pagination,
+    filters,
+    sorter,
+    extra,
+  ) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
 
   return (
     <div>
-    <h3 className="mb-5 text-4xl font-bold">Quản lý người dùng</h3>
-    <Button
+      <h3 className="mb-5 text-4xl font-bold">Quản lý người dùng</h3>
+      <Button
         className="mb-4"
         onClick={() => {
-            dispatch(openModal())
+          dispatch(openModal());
         }}
-    >
+      >
         Thêm quản trị viên
-    </Button>
-    <FormModal />
-    <Search
-                className="mb-4"
-                placeholder="input search text"
-                allowClear
-                enterButton={<IoSearchOutline />}
-                size="large"
-                // onSearch={onSearch}
-                onChange={async (e) => {
-                  if (userRef.current) {
-                      clearTimeout(userRef.current);
-                  }
-                  userRef.current = setTimeout(async () => {
-                      setValue(e.target.value);
-                      console.log('123412',value)
-                      if(value){
-                        dispatch(searchUsersThunk(value))
-                      }else{
-                        dispatch(getUsersThunk())
-                      }
-                  }, 700);
-              }}
-            />
-    <Table
-        columns={columns}
-        dataSource={data}
-        onChange={onChange}
-    />
-</div>
-  )
+      </Button>
+      <FormModal />
+      <Search
+        className="mb-4"
+        placeholder="input search name"
+        allowClear
+        enterButton={<IoSearchOutline />}
+        size="large"
+        onChange={async (e) => {
+          if (userRef.current) {
+            clearTimeout(userRef.current);
+          }
+          userRef.current = setTimeout(async () => {
+          if(e.target.value){
+            dispatch(searchUsersThunk(e.target.value));
+          }else{
+            dispatch(getUsersThunk())
+          }
+          }, 500);
+        }}
+      />
+      <Table columns={columns} dataSource={data} onChange={onChange} />
+    </div>
+  );
 }
