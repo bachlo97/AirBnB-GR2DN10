37c25223666f98765cc83e5d42829cbdde1c50ae
phamduy2112 <duyp7454@gmail.com>
2024-05-31 08:34:01 +0700

    day code

--- src/services/localtion/Localtion.service.ts
+++ src/services/localtion/Localtion.service.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useAppDispatch } from "@/redux/hooks";
 import { axiosWithAuth,axiosWithAuthToken } from "../axios.config"
 import { getAdminLocationThunk } from "@/redux/admin-location/AdminLocation.slice";
@@ -62,48 +61,3 @@ export const delLocation=async(id:any)=>{
         console.log(e.response?.data);
     }
 }
-=======
-import { axiosWithAuth, axiosWithAuthToken } from "../axios.config";
-
-export const getLocaltion = async () => {
-  try {
-    const resp = await axiosWithAuth("/vi-tri");
-    console.log("getLocaltion resp",resp);
-    return resp.data;
-  } catch (e: any) {
-    console.log(e.response?.data);
-  }
-};
-export const putLocaltion = async (id: any, data: any) => {
-  try {
-    return axiosWithAuthToken(`/vi-tri/${id}`, {
-      method: "put",
-      data: data,
-    });
-  } catch (e: any) {
-    console.log(e.response?.data);
-  }
-};
-export const addLocation = async (data: object) => {
-  try {
-    return axiosWithAuthToken("/vi-tri", {
-      method: "post",
-      data: data,
-    });
-  } catch (e: any) {
-    console.log(e.response?.data);
-  }
-};
-export const delLocation = async (id: any) => {
-  try {
-    const resp = await axiosWithAuthToken(`/vi-tri/${id}`, {
-      method: "delete",
-    });
-    const getData = await getLocaltion();
-    console.log("delLocation",getData );
-    return resp.data;
-  } catch (e: any) {
-    console.log(e.response?.data);
-  }
-};
->>>>>>> fa105c6628ed4f06ac37bac9d590d8a55e83f49a
