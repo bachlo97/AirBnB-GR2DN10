874d685bda569dc54a57def109822f27b095f5bc
bachlo97 <cuongpt8997@gmail.com>
2024-08-22 21:34:43 +0700

    change cyber token

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -5,7 +5,8 @@ export const USER_ID = "user_id";
 export const ROOM_FILTER = "room_filter";
 export const COUNT_FILTER = "count_filter";
 export const TOKEN_CYBER =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBETjEwIiwiSGV0SGFuU3RyaW5nIjoiMjIvMDgvMjAyNCIsIkhldEhhblRpbWUiOiIxNzI0Mjg0ODAwMDAwIiwibmJmIjoxNjkzMTU1NjAwLCJleHAiOjE3MjQ0MzI0MDB9.JjDqstSevsNiLhXlkGx2wJPz6otbfBWxfaCHMzBpb6s";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlSlMgNDIiLCJIZXRIYW5TdHJpbmciOiIwMy8xMi8yMDM0IiwiSGV0SGFuVGltZSI6IjE3MzMxODQwMDAwMDAiLCJpYXQiOjE3MTg2Mjc5ODEsImV4cCI6MTcxODYyODI4MX0.TpIyvhyNppMUf_LED4PmF50733Qqu4Dkp0NGPP5LWro";
+
 export const REDIRECT_AFTER_LOGIN = "redirect_after_Login";
 export const NAME_REGEX =
   /^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễếệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ ]+$/u;
